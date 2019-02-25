0a1ad8c6176a45cf89cf79bc77fcec61706f1d4a
CNuge <nugentc@uoguelph.ca>
2019-02-25 07:20:04 -0500

    first go of url builder

--- bold/url_build.go
+++ bold/url_build.go
@@ -5,8 +5,17 @@ import (
 	"strings"
 )
 
+// take a paramater specified and check that it is a valid paramater
+// for the given 
+func validateParam(param string, data_type string) (bool, error) {
 
-func
+	// check if the param is in the valid inputs for the specified data_type
+	// if so, return true, nil
+	// if not return an error saying that:
+	//  'param' not supported for BOLD query of type: 'data_type'
+
+
+}
 
 
 // take the data type and a map of all the url component paramaters
@@ -26,13 +35,27 @@ func BoldURL(data_type string, params map[string]string) string {
 	}else if data_type == "combined"{
 		url_dtype = "combined?"		
 	}else if data_type == "trace"{
-		url_dtype = "trace?"
-		
+		url_dtype = "trace?"	
 	}
 
-	
+	url_params := []string
 
+	for k , v := range params {
 
+		_, err:= validateParams(k, data_type)
+		if err != nil{
+			log.Fatal(err)
+		}
 
+		param_str := fmt.Sprintf("%v=%v", k , v) 
+
+		url_params = append(url_params, param_str)
+	}
+	// join all the params together
+	strings.Join(url_params, "&")
+	// patch the three components together
+	url := []string{base, url_dtype, url_params}
+	// return the url string
+	return strings.Join(url, "")
 
 }
