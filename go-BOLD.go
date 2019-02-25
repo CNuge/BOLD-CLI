e167a74d76bebecf7402b1c0f2e948564099b825
CNuge <nugentc@uoguelph.ca>
2019-02-25 07:02:26 -0500

    what the program needs to take as inputs

--- go-BOLD.go
+++ go-BOLD.go
@@ -9,4 +9,28 @@ import (
 
 
 
+func main(){
+
+	/*
+
+	set up an argparse that will take the following input styles
+
+	- --summary
+	- --specimen
+	- --sequence
+	- --combined
+	- --trace
+
+	each of which can be followed by a string of identifiers to be queried (in quotes)
+	or a filename which has a line by line list of all the identifiers to grab
+
+	-o --output filename for the output
+	-f --format the format type (csv/tsv... see all options) for the output
+
+	*/
+}
+
+
+
+
 
